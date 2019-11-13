a77124e7aa819682728d5ae46d3edff584fc45d2
emurawski <emil.murawski@gmail.com>
2019-11-13 12:51:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,13 @@ import java.awt.*;
 
 public class Main {
     private static final int DELAY = 10000;
+    private static int x = 0;
 
     public static void main(String... args) throws Exception {
         Robot robot = new Robot();
         while (true) {
             Point location = MouseInfo.getPointerInfo().getLocation();
-            robot.mouseMove((int) location.getX(), (int) location.getY() - 1);
-            Thread.sleep(DELAY);
-            robot.mouseMove((int) location.getX(), (int) location.getY() + 1);
+            robot.mouseMove((int) location.getX(), (int) location.getY() + (x++ % 2 == 0 ? 1 : 0));
             Thread.sleep(DELAY);
         }
     }
